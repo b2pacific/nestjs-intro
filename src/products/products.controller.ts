03301037957c10d956bfd6808a811950ddcd9d45
b2pacific <b2pacific@gmail.com>
2020-07-29 11:13:33 +0530

    Add all remaining routes

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Patch, Delete } from '@nestjs/common';
 import { ProductsService } from './products.service';
 
 @Controller("products")
@@ -10,5 +10,27 @@ export class ProductsController {
     addProduct(@Body("title") prodTitle: string, @Body("description") prodDesc: string, @Body("price") prodPrice: number): any {
         const Id = this.productsService.insertProduct(prodTitle, prodDesc, prodPrice);
         return {Id: Id};
+    }
+
+    @Get()
+    getAllProducts(): any {
+        return this.productsService.getProducts();
+    }
+
+    @Get(":id")
+    getProduct(@Param("id") prodId: string): any {
+        return this.productsService.getSingleProduct(prodId);
+    }
+
+    @Patch(":id")
+    updateProduct(@Param("id") prodId: string, @Body("title") prodTitle: string, @Body("description") prodDesc: string, @Body("price") prodPrice: number): any{
+        this.productsService.updateProduct(prodId, prodTitle, prodDesc, prodPrice);)
+        return null;
+    }
+
+    @Delete(":id")
+    removeProduct(@Param("id") prodId: string) {
+        this.productsService.removeProduct(prodId);
+        return null;
     }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,15 +1,60 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { Product } from './products.model';
 
 @Injectable()
 
 export class ProductsService {
-    products: Product[] = [];
+    private products: Product[] = [];
 
     insertProduct(title: string, desc: string, price: number): string{
-        const prodId = new Date().toString();
+        const prodId = Math.random().toString();
         const newProduct = new Product(prodId, title, desc, price);
         this.products.push(newProduct);
         return prodId;
+    }
+
+    getProducts(): Product[] {
+        return [...this.products];
+    }
+
+    getSingleProduct(ProdId: string): any {
+        const product = this.products.find((prod) => prod.id === ProdId);
+        if(!product){
+            throw new NotFoundException("Could not found product");
+        }
+        return {...product};
+    }
+
+    updateProduct(ProdId: string, title: string, desc: string, price: number): any {
+        const ind = this.products.findIndex((prod) => prod.id === ProdId);
+        const product = this.products[ind];
+        if(!product){
+            throw new NotFoundException("Could not found product");
+        }
+        else {
+            const updatedProduct = {...product};
+            if(title){
+                updatedProduct.title = title;
+            }
+
+            if(desc){
+                updatedProduct.description = desc;
+            }
+
+            if(price){
+                updatedProduct.price = price;
+            }
+        }
+    }
+
+    removeProduct(ProdId: string) {
+        const ind = this.products.findIndex((prod) => prod.id === ProdId);
+        const product = this.products[ind];
+        if(!product){
+            throw new NotFoundException("Could not found product");
+        }
+        else {
+            this.products.splice(ind, 1);
+        }
     }
 }
